472e6e8f27a05475f8c7efab2ccf19944f5cc2fc
SilasBorges <bsilas871@hotmail.com>
2023-07-06 20:59:05 -0300

    Creating Layouts and splash

--- app/src/main/java/com/companySilas/moviesdb/presentation/MainActivity.kt
+++ app/src/main/java/com/companySilas/moviesdb/presentation/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.companySilas.moviesdb.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.companySilas.moviesdb.R
+import com.companySilas.moviesdb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
---
+++ app/src/main/java/com/companySilas/moviesdb/presentation/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.companySilas.moviesdb.presentation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.companySilas.moviesdb.R
+import com.companySilas.moviesdb.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, TIME_SPLASH.toLong())
+    }
+
+    companion object {
+        const val TIME_SPLASH = 2000
+    }
+}
